6e4fa4deaa2986cc031f8f8c29455e52a2234e83
BrunoTrakinas <brunowebdesing@gmail.com>
2025-09-26 22:33:41 -0300

    login3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,2 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  darkMode: "class", // <- ESSENCIAL pro toggle via classe "dark" no <html>
-  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {}
-  },
-  plugins: []
-};
+export default {};
